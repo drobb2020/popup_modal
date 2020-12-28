63b99a11b974a7c4e245ec50dd65108b354191c2
David Robb <66834033+drobb2020@users.noreply.github.com>
2020-12-28 11:49:47 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// Get modal elements
+const modal = document.getElementById("simpleModal");
+
+// Get open modal button
+const modalBtn = document.getElementById("modal-btn");
+
+// Get close modal element
+const closeBtn = document.querySelector(".closeBtn");
+
+//Event listeners
+modalBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+window.addEventListener("click", clickOutside);
+
+// Open modal function
+function openModal() {
+  modal.style.display = "block";
+}
+function closeModal() {
+  modal.style.display = "none";
+}
+function clickOutside(e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+}
